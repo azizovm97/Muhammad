19bebeccea5d2766d48a2f49fd35cbe7897d5508
azizovm97 <azizovm97@gmail.com>
2022-01-31 20:03:23 +0500

    add function orderFish

--- Validation/orderFish.php
+++ Validation/orderFish.php
@@ -30,14 +30,18 @@ function validateInt($fish_count)
     return "Type of fish number is ok. ";
 }
 
- 
-function checkingMinMax(string $fish_name)
+
+function checkingMax(string $fish_name)
 {
-    $min = 2;
     $max = 10;
     if (strlen($fish_name) >= $max) {
         return "There are many characters in the name of the fish!"; //В названии рыбы много символов!.
     }
+    return "The number of characters is enough!"; //Количество символов достаточно!
+}
+function checkingMin(string $fish_name)
+{
+    $min = 2;
     if (strlen($fish_name) < $min) {
         return "There are few characters in the name of the fish!"; //В названии рыбы мало символов!
     }
@@ -45,7 +49,7 @@ function checkingMinMax(string $fish_name)
 }
 
 
-function searchList(string $fish_name, array $fishes_list)
+function searchInList(string $fish_name, array $fishes_list)
 {
     if (!in_array($fish_name, $fishes_list)) {
         return "There is no such fish on the list! "; //В списке не такой рыбы!
@@ -70,9 +74,10 @@ function orderFish(string $fish_name, string $fish_count)
     ];
     $type_checking_str = validateStr($fish_name);
     $type_checking_int = validateInt($fish_count);
-    $min_max_checking  = checkingMinMax($fish_name);
-    $list_search       = searchList($fish_name, $fishes_list);
-    return "$type_checking_str $type_checking_int $min_max_checking $list_search";
+    $min_checking      = checkingMin($fish_name);
+    $max_checking      = checkingMax($fish_name);
+    $list_search       = searchInList($fish_name, $fishes_list);
+    return "$type_checking_str $type_checking_int $min_checking $max_checking $list_search";
 }
 //Business Logic
 $fish_name  = readline("Enter fish name: "); //Введите название рыбы
