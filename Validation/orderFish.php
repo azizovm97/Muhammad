12dc52ba3e795fb23e14a95fb8440854cf2fed81
Muhammad <azizovm97@gmail.com>
2022-02-01 21:54:26 +0500

    edit min & max

--- Validation/orderFish.php
+++ Validation/orderFish.php
@@ -1,20 +1,4 @@
 <?php
-//Задача:"Заказ у рыбака"
-//У мостика корабля забросил свою удочку наш рыбак. 
-//Он готов поймать одну рыбу для вас, но он требует писать свой заказ правильно. 
-//Напишите функцию, которая поверяет заказ для рыбака, 
-//в случае ошибки вернуть сообщение (422-неверно отправленные данные N)
-
-//Список рыб у рыбака:
-//1. mackerel - скумбрия
-//2. trout — форель
-//3. tuna — тунец
-//4. vobla — вобла
-//5. pikeperch — судак
-//6. herring — сельдь
-//7. catfish — сом
-
-
 function validateStr($fish_name)
 {
     if ((int) $fish_name != 0) {
@@ -31,30 +15,28 @@ function validateInt($fish_count)
 }
 
 
-function checkingMax(string $fish_name)
+function validateMax(string $fish_name, $max_value)
 {
-    $max = 10;
-    if (strlen($fish_name) >= $max) {
-        return "There are many characters in the name of the fish!"; //В названии рыбы много символов!.
+    if (strlen($fish_name) >= $max_value) {
+        return "There are many characters in the name of the fish!"; 
     }
-    return "The number of characters is enough!"; //Количество символов достаточно!
+    return "The number of characters is enough!"; 
 }
-function checkingMin(string $fish_name)
+function validateMin(string $fish_name, $min_value)
 {
-    $min = 2;
-    if (strlen($fish_name) < $min) {
-        return "There are few characters in the name of the fish!"; //В названии рыбы мало символов!
+    if (strlen($fish_name) <= $min_value) {
+        return "There are few characters in the name of the fish!"; 
     }
-    return "The number of characters is enough!"; //Количество символов достаточно!
+    return "The number of characters is enough!"; 
 }
 
 
 function searchInList(string $fish_name, array $fishes_list)
 {
     if (!in_array($fish_name, $fishes_list)) {
-        return "There is no such fish on the list! "; //В списке не такой рыбы!
+        return "There is no such fish on the list! "; 
     }
-    return "There is such a fish on the list!"; //В списке есть такая рыба!
+    return "There is such a fish on the list!"; 
 }
 
 
@@ -72,14 +54,14 @@ function orderFish(string $fish_name, string $fish_count)
         "herring",
         "catfish",
     ];
-    $type_checking_str = validateStr($fish_name);
-    $type_checking_int = validateInt($fish_count);
-    $min_checking      = checkingMin($fish_name);
-    $max_checking      = checkingMax($fish_name);
+    $type_validate_str = validateStr($fish_name);
+    $type_validate_int = validateInt($fish_count);
+    $min_validate      = validateMin($fish_name, 3);
+    $max_validate      = validateMax($fish_name, 10);
     $list_search       = searchInList($fish_name, $fishes_list);
-    return "$type_checking_str $type_checking_int $min_checking $max_checking $list_search";
+    return "$type_validate_str $type_validate_int $min_validate $max_validate $list_search";
 }
 //Business Logic
-$fish_name  = readline("Enter fish name: "); //Введите название рыбы
-$fish_count = readline("Enter number of fish: "); //Введите количество рыбы
+$fish_name  = readline("Enter fish name: "); 
+$fish_count = readline("Enter number of fish: ");
 echo orderFish($fish_name, $fish_count);
